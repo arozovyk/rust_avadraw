dc2ab11f890eac0b372d40ada4420a24b9306821
arozovyk <artemiy.rozovyk@ocamlpro.com>
2022-11-17 16:40:28 +0100

    simplify main

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,17 @@ mod common;
 mod crawler;
 mod image_renderer;
 
-use tokio::runtime::Runtime;
-
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv::dotenv().ok();
-    let rt = Runtime::new().unwrap();
-    let handle = rt.spawn(async move {
-        tokio::spawn(async {
-            crawler::run().await;
-        });
-        // It working only because image renderer loops
-        // Ending this task will also drop the crawler task
-        tokio::spawn(async {
-            image_renderer::run().await;
-        })
-        .await
-        .unwrap();
+    let crawler_handle = tokio::spawn(async {
+        crawler::run().await;
     });
-    println!("Crawler and IR have been launched in a tokio runtime");
-    rt.block_on(async move {
-        handle.await.unwrap();
-    })
-    /*  let ir_handle = thread::spawn(move || image_renderer::run());
-    ir_handle.join().unwrap(); */
+    let ir_handler = tokio::spawn(async {
+        image_renderer::run().await;
+    });
+    crawler_handle.await.unwrap();
+    ir_handler.await.unwrap();
+
     // TODO launch an api server here.
 }
